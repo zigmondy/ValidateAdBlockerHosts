0bee7abb285c585f3a1127c784c5e9a1b1a32275
zigmondy <ratheenitin@hotmail.com>
2017-05-12 21:48:32 -0700

    update

---
+++ ValidateAdBlockerHosts/ValidateAdBlockerHosts/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+namespace ValidateAdBlockerHosts
+{
+    using System.Net;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var writer = new StreamWriter("out.txt"))
+            {
+                using (var reader = new StreamReader("in.txt"))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (line.StartsWith("0.0.0.0"))
+                        {
+                            writer.WriteLine(line);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
